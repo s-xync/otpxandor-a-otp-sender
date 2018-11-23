d403ebf590adec62eb63bc4d27f7abde17b77813
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-11-24 00:11:43 +0530

    adds get endpoint for getting contacts information

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,12 +5,32 @@ const Otp = require('../models/otp.js');
 
 const apiRouter = express.Router();
 
-apiRouter.get('/test',
+apiRouter.get('/contacts',
 (req, res) => {
-  return res.json({
-    success : true,
-    message : "Test successful"
+  const query = {
+    isDeleted : false
+  };
+  Contact.find(query, (err, contacts) => {
+    if(err){
+      return res.json({
+        success : false,
+        message : "Internal server error"
+      });
+    }else{
+      let contactsObj = {};
+      contacts.forEach((contact) => {
+        const { _id, firstName, lastName, phoneNumber } = contact;
+        const id = _id;
+        contactsObj[id] = { id, firstName, lastName, phoneNumber };
+      });
+      return res.json({
+        success : true,
+        message : "Contacts retrieved successfully",
+        contacts : contactsObj
+      });
+    }
   });
 });
+
 
 module.exports = apiRouter;
