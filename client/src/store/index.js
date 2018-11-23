7822f1dcc6623e95769f2c97799da89ee95c4de9
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-11-24 04:33:34 +0530

    basic boiler plate of store and reducer

---
+++ client/src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
+
+const middleware = [thunk];
+
+/*
+****
+If you have redux dev tool extension in your browser, then you can uncomment the below line and comment the other one.
+****
+*/
+
+const store = createStore(rootReducer, compose(applyMiddleware(...middleware) , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+// const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
+
+export default store;
