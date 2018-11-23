e0a529b97c786e46891acb5630b7b158fb0a86da
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-11-23 23:03:49 +0530

    creates models for contacts and otps

--- server/models/contact.js
+++ server/models/contact.js
@@ -1,7 +1,29 @@
 const mongoose = require('mongoose');
 
 const contactSchema = mongoose.Schema({
-
+  firstName : {
+    type : String,
+    required : true
+  },
+  lastName : {
+    type : String,
+    required : true
+  },
+  phoneNumber : {
+    type : String,
+    required : true,
+    unique : true
+  },
+  createdOn : {
+    type : Date,
+    default : Date.now(),
+    required : true
+  },
+  isDeleted : {
+    type : Boolean,
+    default : false,
+    required : true
+  }
 });
 
 const Contact = module.exports = mongoose.model('Contact', contactSchema);
--- server/models/otp.js
+++ server/models/otp.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose');
 
 const otpSchema = mongoose.Schema({
-
+  body : {
+    type : String,
+    required : true
+  },
+  contactID : {
+    type : mongoose.Schema.Types.ObjectId,
+    required : true
+  },
+  sentOn : {
+    type : Date,
+    default : Date.now(),
+    required : true
+  },
+  isDeleted : {
+    type : Boolean,
+    default : false,
+    required : true
+  }
 });
 
 const Otp = module.exports = mongoose.model('Otp', otpSchema);
