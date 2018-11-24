7804572d1ad75d3c0e5c9acea69538f5b4824bb2
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-11-24 06:31:08 +0530

    all contacts page now works

---
+++ client/src/components/contactThumbnail.jsx
@@ -0,0 +1,32 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+class ContactThumbnail extends Component{
+  render(){
+    const { contactID } = this.props;
+    const { id, firstName, lastName } = this.props.contacts[contactID];
+    return(
+      <Fragment>
+        <div className="card col-md-4" style={{padding:"0.5%"}}>
+          <div className="card-body">
+            <h5 className="card-title">{firstName + " " + lastName}</h5>
+          <Link to={"/contact/" + contactID} className="card-link">Full Details</Link>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+ContactThumbnail.propTypes = {
+  contactID : propTypes.string.isRequired,
+  contacts : propTypes.object.isRequired
+};
+
+const mapStateToProps = ({ contacts }) => ({
+  contacts : contacts
+});
+
+export default connect(mapStateToProps, {  })(ContactThumbnail);
--- client/src/components/contacts.jsx
+++ client/src/components/contacts.jsx
@@ -1,14 +1,34 @@
 import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 import NavBar from './navBar';
+import ContactThumbnail from './contactThumbnail';
 
 class Contacts extends Component{
   render(){
     return(
       <Fragment>
         <NavBar />
+        <div className="container">
+          <br/>
+          <h1 style={{textAlign : "center"}}>All Contacts</h1>
+          <br/>
+          <div className="row">
+            {this.props.contactsArr.map((contact) => <ContactThumbnail key={contact.id} contactID={contact.id}/>)}
+          </div>
+          <br/>
+        </div>
       </Fragment>
     );
   }
 }
 
-export default Contacts;
+Contacts.propTypes = {
+  contactsArr : propTypes.array.isRequired
+}
+
+const mapStateToProps = ({ contacts }) => ({
+  contactsArr : Object.values(contacts)
+});
+
+export default connect(mapStateToProps, {  })(Contacts);
